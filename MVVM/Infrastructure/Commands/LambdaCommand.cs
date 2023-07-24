a479a7759dae952c61cac7a783d354ce6a0d3628
SychAndrii <97425977+SychAndrii@users.noreply.github.com>
2023-07-24 12:15:19 -0400

    Lambda command class added.

---
+++ MVVM/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,25 @@
+﻿using MVVM.Infrastructure.Commands.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Infrastructure.Commands
+{
+    class LambdaCommand : Command
+    {
+        public LambdaCommand(Action<object?> execute, Func<object, bool>? canExecute = null)
+        {
+            _Execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _CanExecute = canExecute;
+        }
+
+        public Action<object?> _Execute { get; }
+        public Func<object, bool>? _CanExecute { get; }
+
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object? parameter) => _Execute(parameter);
+    }
+}
