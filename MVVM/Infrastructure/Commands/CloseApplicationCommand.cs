f6272809f588db45d062a1f3c4553b1fccf50d4d
SychAndrii <97425977+SychAndrii@users.noreply.github.com>
2023-07-24 12:41:15 -0400

    CloseApplicationCommand as a resource.

---
+++ MVVM/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MVVM.Infrastructure.Commands
+{
+    class CloseApplicationCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => true;
+
+        public void Execute(object? parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
